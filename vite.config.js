f58357ce0bc6644ea93c6c3ac070d4b6bcc72206
yoshihiro-dan <aerialact@gmail.com>
2025-02-05 22:30:12 +0900

    TailwindCSSのバージョンを最新にアップデート

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,32 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import restart from 'vite-plugin-restart'
-import path from 'path';
+import path from 'path'
 
-export default {
-    root: 'src/',  // ソースファイルのルート
-    publicDir: '../static/', // 静的ファイルの置き場
-    server:
-    {
-        host: true,
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env),
-        fs: {
-            allow: ['works', 'works/img', 'works/css', 'works/js']
-        }
-    },
-    build:
-    {
-        assetsDir: 'works/assets', // assets を /works/assets/ に配置
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true,
-        rollupOptions: {
-            input: {
-                main: path.resolve(__dirname, 'src/works/index.html'), // メインページ
-                works: path.resolve(__dirname, 'src/works/skills-achievements.html') // 追加ページ
-            }
-        }
-    },
-    plugins:
-    [
-        restart({ restart: [ '../static/**', ] }) // 静的ファイルの変更時に再起動
-    ],
-}
+export default defineConfig({
+  root: 'src/',  // ソースファイルのルート
+  publicDir: '../static/', // 静的ファイルの置き場
+  server: {
+    host: true,
+    open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env),
+    fs: {
+      allow: ['works', 'works/img', 'works/css', 'works/js']
+    }
+  },
+  build: {
+    assetsDir: 'works/assets', // assets を /works/assets/ に配置
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/works/index.html'), // メインページ
+        works: path.resolve(__dirname, 'src/works/skills-achievements.html') // 追加ページ
+      }
+    }
+  },
+  plugins: [
+    tailwindcss(),  // Tailwind CSS プラグイン
+    restart({ restart: [ '../static/**', ] })  // 静的ファイルの変更時に再起動
+  ]
+})
